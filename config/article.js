aca3ed8189b7a88010c1ec257ed104b295ec3047
Joanna Kao <joanna.kao@ft.com>
2018-02-16 14:52:57 -0500

    add doc and article configs for project

--- config/article.js
+++ config/article.js
@@ -1,20 +1,20 @@
 export default _ => ({ // eslint-disable-line
 
   // link file UUID
-  id: '04715cce-0840-11e8-9650-9c0ad2d7c5b5',
+  id: '6db4dce0-1350-11e8-8cb6-b9ccc4c4dbbb',
 
   // canonical URL of the published page
   // https://ig.ft.com/sites/documents/philip-hammond get filled in by the ./configure script
-  url: 'https://ig.ft.com/documents/gop-memo',
+  url: 'https://ig.ft.com/documents/russia-indictment-mueller-probe/',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
   publishedDate: new Date(),
 
-  headline: 'Read the previously classified Devin Nunes memo',
+  headline: 'Read Mueller\'s election indictment',
 
   // summary === standfirst (Summary is what the content API calls it)
-  summary: 'House Republicans release previously classified memo which alleges FBI surveillance abuses against Donald Trump’s campaign during the 2016 election',
+  summary: 'Special counsel\'s allegations contained in 37 pages of documents',
 
   topic: {
     name: 'Trump Russia ties',
@@ -23,25 +23,25 @@ export default _ => ({ // eslint-disable-line
 
   relatedArticle: {
     text: 'Related article »',
-    url: 'https://www.ft.com/content/53357b54-077a-11e8-9650-9c0ad2d7c5b5',
+    url: 'https://www.ft.com/content/21f8a01a-1347-11e8-8cb6-b9ccc4c4dbbb',
   },
 
   mainImage: {
     title: '',
     description: '',
-    url: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ac959bdaa-0634-11e8-9e12-af73e8db3c71?source=ig&width=2048',
+    // url: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ac959bdaa-0634-11e8-9e12-af73e8db3c71?source=ig&width=2048',
     width: 2048, // ensure correct width
     height: 1152, // ensure correct height
   },
 
   // Byline can by a plain string, markdown, or array of authors
   // if array of authors, url is optional
-  byline: [
-    { name: 'FT reporters', url: '' },
-  ],
+  // byline: [
+    // { name: 'FT reporters', url: '' },
+  // ],
 
   // Appears in the HTML <title>
-  title: 'Read the previously classified Devin Nunes memo',
+  title: 'Read Mueller\'s election indictment',
 
   // meta data
   description: '',
