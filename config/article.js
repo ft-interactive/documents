3294b5b88fbe51f588a6251c3c2952fb12a28172
Fan Fei <feif1021@gmail.com>
2019-04-18 13:16:27 -0400

    added article stuff

--- config/article.js
+++ config/article.js
@@ -1,35 +1,36 @@
 export default _ => ({ // eslint-disable-line
 
   // link file UUID
-  id: '04715cce-0840-11e8-9650-9c0ad2d7c5b5',
+  id: '556b88ea-61ed-11e9-b285-3acd5d43599e',
 
   // canonical URL of the published page
   // https://ig.ft.com/sites/documents/philip-hammond get filled in by the ./configure script
-  url: 'https://ig.ft.com/documents/gop-memo',
+  url: 'https://ig.ft.com/documents/mueller-report',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
   publishedDate: new Date(),
 
-  headline: 'Read the previously classified Devin Nunes memo',
+  headline: 'Read the the complete, searchable Mueller report',
 
   // summary === standfirst (Summary is what the content API calls it)
-  summary: 'House Republicans release previously classified memo which alleges FBI surveillance abuses against Donald Trump’s campaign during the 2016 election',
+  summary: 'The redacted 448-page report, as distributed by the US Justice department',
 
   topic: {
     name: 'Trump Russia ties',
-    url: 'https://www.ft.com/trump-russia-ties',
-  },
+export default _ => ({ // eslint-disable-line
 
   relatedArticle: {
-    text: 'Related article »',
+    text: 'Related article »',
     url: 'https://www.ft.com/content/53357b54-077a-11e8-9650-9c0ad2d7c5b5',
+    url: 'https://www.ft.com/content/21f8a01a-1347-11e8-8cb6-b9ccc4c4dbbb',
   },
 
   mainImage: {
     title: '',
     description: '',
     url: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ac959bdaa-0634-11e8-9e12-af73e8db3c71?source=ig&width=2048',
+    // url: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ac959bdaa-0634-11e8-9e12-af73e8db3c71?source=ig&width=2048',
     width: 2048, // ensure correct width
     height: 1152, // ensure correct height
   },
@@ -39,73 +40,12 @@ export default _ => ({ // eslint-disable-line
   byline: [
     { name: 'FT reporters', url: '' },
   ],
+  // byline: [
+    // { name: 'FT reporters', url: '' },
+  // ],
 
   // Appears in the HTML <title>
-  title: 'Read the previously classified Devin Nunes memo',
+  title: 'The redacted 448-page report, as distributed by the US Justice department',
 
   // meta data
   description: '',
-
-  /*
-  TODO: Select Twitter card type -
-        summary or summary_large_image
-
-        Twitter card docs:
-        https://dev.twitter.com/cards/markup
-  */
-  twitterCard: 'summary',
-
-  /*
-  TODO: Do you want to tweak any of the
-        optional social meta data?
-  */
-  // General social
- socialImage: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Aa4154ae8-0844-11e8-9650-9c0ad2d7c5b5?source=ig&width=2048',
-  // socialHeadline: '',
-  // socialSummary:  '',
-
-  // TWITTER
-  // twitterImage: '',
-  // twitterCreator: '@individual's_account',
-  // tweetText:  '',
-  // twitterHeadline:  '',
-
-  // FACEBOOK
-  // facebookImage: '',
-  // facebookHeadline: '',
-
-  // onwardjourney: {
-  //
-  //   // list (methode list) or topic
-  //   type: 'topic',
-  //
-  //   // topic or list id
-  //   id: 'TnN0ZWluX1BOX1BvbGl0aWNpYW5fNDA1-UE4=',
-  //
-  //   // a heading is provided automatically if not set (peferred)
-  //   heading: 'Philip Hammond',
-  // },
-
-  tracking: {
-
-    /*
-
-    Microsite Name
-
-    e.g. guffipedia, business-books, baseline.
-    Used to query groups of pages, not intended for use with
-    one off interactive pages. If you're building a microsite
-    consider more custom tracking to allow better analysis.
-    Also used for pages that do not have a UUID for whatever reason
-    */
-    // micrositeName: '',
-
-    /*
-    Product name
-
-    This will usually default to IG
-    however another value may be needed
-    */
-    // product: '',
-  },
-});
